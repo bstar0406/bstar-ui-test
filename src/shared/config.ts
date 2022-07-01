3f665f7ba295595a27df517516ee729064cb8f70
bstar0406 <handby256@outlook.com>
2022-07-01 12:48:31 +0900

    https

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,5 @@
 export const apis = {
-    api1:'http://www.omdbapi.com/?s=Matrix&apikey=720c3666',
-    api2:'http://www.omdbapi.com/?s=Matrix%20Reloaded&apikey=720c3666',
-    api3:'http://www.omdbapi.com/?s=Matrix%20Revolutions&apikey=720c3666',
+    api1:'https://www.omdbapi.com/?s=Matrix&apikey=720c3666',
+    api2:'https://www.omdbapi.com/?s=Matrix%20Reloaded&apikey=720c3666',
+    api3:'https://www.omdbapi.com/?s=Matrix%20Revolutions&apikey=720c3666',
 }
