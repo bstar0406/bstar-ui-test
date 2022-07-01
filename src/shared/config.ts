cd3e16eba4e4ea6ac01a2f8ec4d9cf3671dfeff0
bstar0406 <handby256@outlook.com>
2022-07-01 12:37:47 +0900

    api

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,9 +1,4 @@
 export const apis = {
-    // api1:'https://testapi-3.herokuapp.com//v1/test/api1',
-    // api2:'https://testapi-3.herokuapp.com//v1/test/api2',
-    // api3:'https://testapi-3.herokuapp.com//v1/test/api3',
-    // server:'https://testapi-3.herokuapp.com/',
-
     api1:'http://www.omdbapi.com/?s=Matrix&apikey=720c3666',
     api2:'http://www.omdbapi.com/?s=Matrix%20Reloaded&apikey=720c3666',
     api3:'http://www.omdbapi.com/?s=Matrix%20Revolutions&apikey=720c3666',
