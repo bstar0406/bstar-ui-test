9334884c70d9b282c54a6118e5853a8c547ed2cf
bstar0406 <handby256@outlook.com>
2022-07-02 14:25:31 +0900

    table updated

--- src/components/DisplayBoard.tsx
+++ src/components/DisplayBoard.tsx
@@ -1,50 +1,72 @@
-// import { apis } from '../shared/config'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import React from 'react';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'No', width: 70},
+  { field: 'poster', headerName: 'Poster', width: 130, flex:1, renderCell: (item) => {
+    return (
+      <>
+        <img
+          src={(item as any).Poster}
+          width="50px"
+          height="50px"
+          alt="poster"
+        />
+      </>
+    );
+  } },
+  { field: 'title', headerName: 'Title', width: 130, flex:1 },
+  {
+    field: 'type',
+    headerName: 'Type',
+    width: 90,
+    flex:1
+  },
+  {
+    field: 'year',
+    headerName: 'Year',
+    width: 90, 
+    flex:1
+  },
+  {
+    field: 'imdbID',
+    headerName: 'imdbID',
+    width: 90,
+    flex:1
+  },
+];
 
 interface IPropsDisplayBoard {
   data: any
 }
 const DisplayBoard = ({ data }: IPropsDisplayBoard) => {
+  const [rows, setRows] = React.useState<Array<any>>([]);
+  React.useEffect(()=>{
+    let temp:Array<any> = [];
+    if(data){
+      data.map((item: any, index: number) => {
+        temp.push({
+          id:index+1,
+          poster:{item},
+        title:item.Title,
+        type:item.Type,
+        year:item.Year,
+        imdbID:item.imdbID
+        })
+        setRows(temp);
+        return item;
+      })
+    }
+    console.log(data)
+  },[data])
   return (
-    <div className="container">
-      <table className="table table-dark">
-        <thead>
-          <tr>
-            <td>Title</td>
-            <td>Type</td>
-            <td>Year</td>
-            <td>imdbID</td>
-            <td>Poster</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((item: any, index: number) => {
-              return (
-                <tr key={index}>
-                  
-                  <td>{(item as any).Title}</td>
-                  <td>{(item as any).Type}</td>
-                  <td>{(item as any).Year}</td>
-                  <td>{(item as any).imdbID}</td>
-                  <td>
-                    {/* <img
-                      src={apis.server+(item as any).Poster}
-                      width="50px"
-                      height="50px"
-                      alt="poster"
-                    /> */}
-                    <img
-                      src={(item as any).Poster}
-                      width="50px"
-                      height="50px"
-                      alt="poster"
-                    />
-                  </td>
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
+    <div className="container" style={{ height: 500, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+      />
     </div>
   )
 }
